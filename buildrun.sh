94e8aa6db4f1129d684bc3c10bca6dfdf4e3628d
Zeadar <siiigsonik@gmail.com>
2025-05-10 01:30:56 +0200

    support for stdin and arguments

--- buildrun.sh
+++ buildrun.sh
@@ -9,17 +9,23 @@
 # t: clock build time, don't execute
 # no argument: execute
 
+# rest of arguments are passed into ./out < /dev/stdin
+
+# !!! gdb does sadly not pass from stdin by default !!!
+# inside gdb, run:
+# run < /dev/stdin
+
 flags="-std=gnu99 -ggdb3 -Wall -Wextra -O0 -lm"
 compiler="gcc"
 
 if [ "$2" = "d" ]; then
-    $compiler $flags "$1" -o out && gdb ./out
+    $compiler $flags "$1" -o out && shift 2 && gdb --args ./out < /dev/stdin "$@"
 elif [ "$2" = "v" ]; then
-    $compiler $flags "$1" -o out && valgrind ./out
+    $compiler $flags "$1" -o out && shift 2 && valgrind -- ./out < /dev/stdin "$@"
 elif [ "$2" = "t" ]; then
-    time $compiler $flags "$1" -o out
+    time $compiler $flags "$1" -o out "$@"
 else
-    $compiler $flags "$1" -o out && ./out
+    $compiler $flags "$1" -o out && shift && ./out < /dev/stdin "$@"
 fi
 
 rm -f out
