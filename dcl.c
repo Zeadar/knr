d2b1a225bc6fe3e22785272669b8ed8d77d4ece0
Zeadar Sagaena <zeadar@suzune.shrew>
2025-05-20 14:56:46 +0200

    bump

--- dcl.c
+++ dcl.c
@@ -50,4 +50,38 @@ void dirdcl() {
 
     } else if (tokentype == NAME)
         strcpy(name, token);
+
+    while ((type = gettoken()) == PARENS || type == BRACKETS)
+        if (type == PARENS)
+            strcat(out, " function returning");
+        else {
+            strcat(out, " array");
+            strcat(out, token);
+            strcat(out, " of");
+        }
+}
+
+int gettoken(void) {
+    int c, getch(void);
+    void ungetch(int);
+    char *p = token;
+
+    while ((c = getch()) == ' ' || c == '\t');
+
+    if (c == '(') {
+        if ((c = getch()) == ')') {
+            strcpy(token, "()");
+            return tokentype = PARENS;
+        } else {
+            ungetch(c);
+            return tokentype = '(';
+        }
+    } else if (c == '[') {
+        for (*p++ = c; (*p++ = getch()) != ']';);
+        *p = '\0';
+        return tokentype = BRACKETS;
+    } else if (isalpha(c)) {
+        for (*p++ = c; isalnum(c = getch()););
+    }
+    return 0;
 }
