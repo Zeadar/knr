8519d79ec9b5cc055882b9cc8755d8e9bce7a320
Zeadar <siiigsonik@gmail.com>
2025-07-07 17:19:42 +0200

    string padding for faster memmove()

--- string_array.c
+++ string_array.c
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include "slice.c"
 
 #define STEP 4096
+#define PADSIZE sizeof(size_t)
 
 typedef struct string_array {
     char *ptr;
@@ -14,6 +16,10 @@ typedef struct string_array {
 } Sarray;
 
 typedef size_t str_index;
+
+static size_t padsize(size_t bytesize) {
+    return (bytesize + PADSIZE - 1) & ~(PADSIZE - 1);
+}
 
 Sarray sarray_create() {
     Sarray sarray = {
@@ -35,53 +41,40 @@ slice_index sarray_size(const Sarray *sarray) {
 }
 
 void sarray_check_grow(Sarray *sarray, size_t bytes) {
-    if (bytes + sarray->head < sarray->end)
+    if (bytes + sarray->head <= sarray->end)
         return;
 
-    sarray->end += STEP + (bytes / STEP) * STEP;
+    sarray->end += ((bytes + STEP - 1) & ~(STEP - 1));
     sarray->ptr = realloc(sarray->ptr, sarray->end);
 }
 
 slice_index sarray_push(Sarray *sarray, const char *str) {
     size_t bytes = strlen(str) + 1;
 
-    sarray_check_grow(sarray, bytes);
+    sarray_check_grow(sarray, padsize(bytes));
 
     char *head = sarray->ptr + sarray->head;
-
-    while (*str != '\0')
-        *head++ = *str++;
-
-    *head++ = '\0';
+    memcpy(head, str, bytes);
 
     slice_index index = slice_push(&sarray->table, &sarray->head);
-    sarray->head += bytes;
+    sarray->head += padsize(bytes);
 
     return index;
 }
 
 void sarray_remove(Sarray *sarray, slice_index index) {
     char *culprit = sarray_get(sarray, index);
-    size_t bytes = strlen(culprit) + 1;
+    size_t bytes = padsize(strlen(culprit) + 1);
 
-    char *write = culprit;
-    char *read = culprit + bytes;
-
-    while (read != sarray->ptr + sarray->head) {
-        *write = *read;
-
-        if (*write == '\0') {
-            str_index here = (write + 1) - sarray->ptr;
-            slice_replace(&sarray->table, ++index, &here);
-        }
-
-        ++write;
-        ++read;
-    }
-
+    memmove(culprit, culprit + bytes, sarray->head - bytes);
     sarray->head -= bytes;
 
     slice_remove(&sarray->table, index);
+
+    while (index != slice_size(&sarray->table)) {
+        str_index *si = slice_get_ptr(&sarray->table, index++);
+        *si -= bytes;
+    }
 }
 
 void sarray_destroy(Sarray *sarray) {
